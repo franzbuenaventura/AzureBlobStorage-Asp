accf0b8bf6909a7d5982d4fb965989a1b99f9607
Franz Justin Buenaventura <franz_buenaventura@hotmail.com>
2015-12-09 14:49:48 +0800

    Initial

---
+++ BlobStorage-ASP.Net/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace BlobStorage_ASP.Net
+{
+    public class Main
+    {
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+    CloudConfigurationManager.GetSetting("StorageConnectionString"));
+    }
+}
