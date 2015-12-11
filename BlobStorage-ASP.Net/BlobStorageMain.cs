a580c466dab37cf5277334324a292fbbc75608bd
Franz Justin Buenaventura <franz_buenaventura@hotmail.com>
2015-12-11 22:29:45 +0800

    Added Upload from Stream and Path File

---
+++ BlobStorage-ASP.Net/BlobStorageMain.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+
+namespace BlobStorage_ASPNet
+{
+    public class BlobStorageMain
+    {
+        public string BlobName { get; set; }
+        public CloudBlobContainer CloudContainer { get; set; }
+
+        public BlobStorageMain(string connectionString, string container)
+        {
+
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudContainer = blobClient.GetContainerReference(container);
+            CloudContainer.CreateIfNotExists();
+        }
+
+        public string UploadCreateOverwriteFile(Stream stream)
+        {
+            try
+            {
+                // Retrieve reference to a blob and use BlobName as a name
+                CloudBlockBlob blockBlob = CloudContainer.GetBlockBlobReference(BlobName);
+                blockBlob.UploadFromStream(stream);
+                return blockBlob.Uri.ToString();
+            }
+            catch (Exception)
+            {
+                return "Failed Upload";
+            }
+        }
+
+        public string UploadCreateOverwriteFile(string path)
+        {
+            try
+            {
+                CloudBlockBlob blockBlob = CloudContainer.GetBlockBlobReference(BlobName);
+                //path - The path of the file that will be uploaded
+                using (var fileStream = System.IO.File.OpenRead(path))
+                {
+                    blockBlob.UploadFromStream(fileStream);
+                }
+                return blockBlob.Uri.ToString();
+            }
+            catch (Exception)
+            {
+                return "Failed Upload";                    
+            }
+        }
+
+
+
+
+    }
+}
---
+++ UnitTestProject1/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+using BlobStorage_ASPNet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Unit
+    {
+        BlobStorageMain blob = new BlobStorageMain(ConfigurationManager.AppSettings["BlobKey"], "test");
+    
+        [TestMethod]
+        public void UploadCreateOverwriteFileTest()
+        {
+            blob.BlobName = "test1";
+            var temp = blob.UploadCreateOverwriteFile(@"../../testImage.jpg");
+            Trace.WriteLine(temp);
+            // Assert
+            Assert.AreNotEqual(temp,"Failed Upload");
+        }
+
+    }
+}
