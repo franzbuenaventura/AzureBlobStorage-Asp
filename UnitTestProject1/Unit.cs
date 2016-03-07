9d6542d4de1d2448b711005592131b26b27f8ab9
Franz Justin Buenaventura <franz_buenaventura@hotmail.com>
2016-03-06 17:40:30 -0800

    update

--- UnitTestProject1/Unit.cs
+++ UnitTestProject1/Unit.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Diagnostics;
+using System.IO;
 using BlobStorage_ASPNet;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,13 +12,23 @@ namespace UnitTestProject1
         BlobStorageMain blob = new BlobStorageMain(ConfigurationManager.AppSettings["BlobKey"], "test");
     
         [TestMethod]
-        public void UploadCreateOverwriteFileTest()
+        public void UploadCreateOverwriteFile_TestPath()
         {
             blob.BlobName = "test1";
             var temp = blob.UploadCreateOverwriteFile(@"../../testImage.jpg");
-            Trace.WriteLine(temp);
+            Trace.WriteLine("Path Test: "+temp);
             // Assert
             Assert.AreNotEqual(temp,"Failed Upload");
+        }
+        [TestMethod]
+        public void UploadCreateOverwriteFile_TestStream()
+        {
+            blob.BlobName = "test1";
+            FileStream stream = new FileStream(@"../../testImage.jpg", FileMode.Open);
+            var temp = blob.UploadCreateOverwriteFile(stream);
+            Trace.WriteLine("Stream Test: "+temp);
+            // Assert
+            Assert.AreNotEqual(temp, "Failed Upload");
         }
 
     }
